3cfb107eee9312eaf46a252242d9418d1150565c
GustavoMarquez <gustavomarquez@refera.com.br>
2022-09-06 13:56:22 -0300

    add scripts e tox

---
+++ scripts/test_sample.py
@@ -0,0 +1,12 @@
+# content of test_sample.py
+def func(x):
+    return x + 1
+
+
+def test_answer():
+    assert func(3) == 5
+
+notas = [0, 0, 9.0, 8.0, 5.0, 10.0, 7.0,
+         7.5, 4.0, 10.0, 7.0, 7.0, 8.0, 8.0, 7.5]
+soma_das_notas = sum(notas)
+print(soma_das_notas)
